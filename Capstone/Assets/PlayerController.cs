0939afa6d26910853a7fd9e556c1b3ecd7fa2fd3
Elliot McGonigal <elliot@elliotmcgonigal.com>
2022-12-17 08:42:00 -0800

    add movement to player

--- Capstone/Assets/PlayerController.cs
+++ Capstone/Assets/PlayerController.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PlayerController : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [Header("References")]
+    public Rigidbody player;
+    public Transform head;
+    public Camera camera;
+
+    [Header("Configurations")]
+    public float walkSpeed;
+    public float runSpeed;
+
+    [Header("Runtime")]
+    Vector3 newVelocity;
+
+    void Update() {
+        newVelocity = Vector3.up * rb.velocity.y;
+        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+        newVelocity.x = Input.GetAxis("Horizontal") * speed;
+        newVelocity.z = Input.GetAxis("Vertical") * speed;
+
+        rb.velocity = transform.TransformDirection(newVelocity);
     }
 }

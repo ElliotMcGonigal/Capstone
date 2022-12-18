d08b322e347152a82f55b69dcb5b7c3bd6148a25
Elliot McGonigal <elliot@elliotmcgonigal.com>
2022-12-18 03:45:05 -0800

    add interaction with items

--- Capstone/Assets/PlayerController.cs
+++ Capstone/Assets/PlayerController.cs
@@ -12,9 +12,15 @@ public class PlayerController : MonoBehaviour {
     [Header("Configurations")]
     public float walkSpeed;
     public float runSpeed;
+    public float jumpSpeed;
+    public float itemPickupDistance;
+    Transform attachedObject = null;
+    float attachedDistance = 0;
 
     [Header("Runtime")]
     Vector3 newVelocity;
+    bool isGrounded = false;
+    bool isJumping = false;
 
     void Start() {
         Cursor.visible = false;
@@ -27,7 +33,55 @@ public class PlayerController : MonoBehaviour {
         float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
         newVelocity.x = Input.GetAxis("Horizontal") * speed;
         newVelocity.z = Input.GetAxis("Vertical") * speed;
+
+        if (isGrounded) {
+            if (Input.GetKeyDown(KeyCode.Space) && !isJumping) {
+                newVelocity.y = jumpSpeed;
+                isJumping = true;
+            }
+        }
+
         player.velocity = transform.TransformDirection(newVelocity);
+
+        RaycastHit hit;
+        bool cast = Physics.Raycast(head.position, head.forward, out hit, itemPickupDistance);
+
+        if (Input.GetKeyDown(KeyCode.E)) {
+            if (attachedObject != null) {
+                attachedObject.SetParent(null);
+                if (attachedObject.GetComponent<Rigidbody>() != null)
+                    attachedObject.GetComponent<Rigidbody>().isKinematic = false;
+
+                if (attachedObject.GetComponent<Collider>() != null)
+                    attachedObject.GetComponent<Collider>().enabled = true;
+
+                attachedObject = null;
+            }
+
+            else {
+                if (cast) {
+                    if (hit.transform.CompareTag("pickable")) {
+                        attachedObject = hit.transform;
+                        attachedObject.SetParent(transform);
+
+                        attachedDistance = Vector3.Distance(attachedObject.position, head.position);
+
+                        if (attachedObject.GetComponent<Rigidbody>() != null)
+                            attachedObject.GetComponent<Rigidbody>().isKinematic = true;
+
+                        if (attachedObject.GetComponent<Collider>() != null)
+                            attachedObject.GetComponent<Collider>().enabled = false;
+                    }
+                }
+            }
+        }
+    }
+
+    void FixedUpdate() {
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 1f)) {
+            isGrounded = true;
+        }
+        else isGrounded = false;
     }
 
     void LateUpdate() {
@@ -35,6 +89,11 @@ public class PlayerController : MonoBehaviour {
         e.x -= Input.GetAxis("Mouse Y") * 2f;
         e.x = RestrictAngle(e.x, -89f, 89f);
         head.eulerAngles = e;
+
+        if (attachedObject != null) {
+            attachedObject.position = head.position + head.forward * attachedDistance;
+            attachedObject.Rotate(transform.right * Input.mouseScrollDelta.y * 15f, Space.World);
+        }
     }
 
     public static float RestrictAngle(float angle, float angleMin, float angleMax) {
@@ -49,5 +108,17 @@ public class PlayerController : MonoBehaviour {
             angle = angleMin;
 
         return angle;
+    }
+
+    void OnCollisionStay(Collision col) {
+        if (Vector3.Dot(col.GetContact(0).normal, Vector3.up) <= .6f)
+            return;
+
+        isGrounded = true;
+        isJumping = false;
+    }
+
+    void OnCollisionExit(Collision col) {
+        isGrounded = false;
     }
 }

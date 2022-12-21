8dcd08701dcff54c61ac9aa5f67e08054e9e7dbb
Elliot McGonigal <elliot@elliotmcgonigal.com>
2022-12-21 15:57:43 -0800

    fix dragon no longer does barrel rolls at mach 20

--- Capstone/Assets/DragonCircle.cs
+++ Capstone/Assets/DragonCircle.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 public class DragonCircle : MonoBehaviour
 {
     float timeCounter = 0;
+    float speed;
+    float width;
     void Start()
     {
-        
+        speed = .1f;
+        width = 18f;
     }
 
     void Update()
     {
-        timeCounter += Time.fixedDeltaTime;
+        timeCounter += Time.fixedDeltaTime*speed;
 
-        float x = Mathf.Cos(timeCounter);
+        float x = (Mathf.Cos(timeCounter))*width;
         float y = 5;
-        float z = Mathf.Sin(timeCounter);
+        float z = (Mathf.Sin(timeCounter))*width;
+        float xx = (Mathf.Cos(timeCounter + speed))*width;
+        float zz = (Mathf.Cos(timeCounter + speed))*width;
 
         transform.position = new Vector3(x, y, z);
+        transform.LookAt(new Vector3(xx, y, zz));
     }
 }
